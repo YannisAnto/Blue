a748ac7b3d9d7a68ed20bc225b84ad86d269e026
YannisAnto <129043024+YannisAnto@users.noreply.github.com>
2023-04-03 13:05:45 +0300

    This is my Blue email template
    This is my first template built on the Maizzle framework!

---
+++ config.js
@@ -0,0 +1,26 @@
+/*
+|-------------------------------------------------------------------------------
+| Development config                      https://maizzle.com/docs/environments
+|-------------------------------------------------------------------------------
+|
+| The exported object contains the default Maizzle settings for development.
+| This is used when you run `maizzle build` or `maizzle serve` and it has
+| the fastest build time, since most transformations are disabled.
+|
+*/
+
+module.exports = {
+  baseURL: 'https://modulescomposer.s3.us-east-2.amazonaws.com/blue/',
+  build: {
+    templates: {
+      source: 'src/templates',
+      destination: {
+        path: 'build_local',
+      },
+      assets: {
+        source: 'src/images',
+        destination: 'images',
+      },
+    },
+  },
+}
